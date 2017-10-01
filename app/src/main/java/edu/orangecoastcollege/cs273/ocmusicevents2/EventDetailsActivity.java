a62bf54432628e6f9b9a66f873ebf75c7a39d7f9
Derek <dphtran501@yahoo.com>
2017-09-30 23:58:12 -0700

    finished EventDetailsActivity.java

--- app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventDetailsActivity.java
@@ -13,39 +13,72 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class EventDetailsActivity extends AppCompatActivity {
+/**
+ * This activity displays information about an artist and the event he/she is performing at. The
+ * information is specified by which list item was clicked in <code>EventsListActivity</code>.
+ *
+ * @author Derek Tran
+ * @version 1.0
+ * @since September 26, 2017
+ */
+public class EventDetailsActivity extends AppCompatActivity
+{
 
+    /**
+     * Initializes <code>EventDetailsActivity</code> by inflating its UI.
+     *
+     * @param savedInstanceState Bundle containing the data it recently supplied in
+     *                           onSaveInstanceState(Bundle) if activity was reinitialized after
+     *                           being previously shut down. Otherwise it is null.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-
         Intent intent = getIntent();
         String title = intent.getStringExtra("Title");
-        String details = intent.getStringExtra("Details");
-        String imageFileName = title.replace(" ", "") + ".jpeg";
+        String date = intent.getStringExtra("Date");
+        String day = intent.getStringExtra("Day");
+        String time = intent.getStringExtra("Time");
+        String location = intent.getStringExtra("Location");
+        String address1 = intent.getStringExtra("Address1");
+        String address2 = intent.getStringExtra("Address2");
+        String imageName = intent.getStringExtra("ImageName");
 
         ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
         TextView eventTitleTextView = (TextView) findViewById(R.id.eventTitleTextView);
-        TextView eventDetailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+        TextView eventDateDayTextView = (TextView) findViewById(R.id.eventDateDayTextView);
+        TextView eventTimeTextView = (TextView) findViewById(R.id.eventTimeTextView);
+        TextView eventLocationTextView = (TextView) findViewById(R.id.eventLocationTextView);
+        TextView eventAddress1TextView = (TextView) findViewById(R.id.eventAddress1TextView);
+        TextView eventAddress2TextView = (TextView) findViewById(R.id.eventAddress2TextView);
 
         AssetManager am = this.getAssets();
-        try {
-            InputStream stream = am.open(imageFileName);
+        try
+        {
+            InputStream stream = am.open(imageName);
             Drawable image = Drawable.createFromStream(stream, title);
             eventImageView.setImageDrawable(image);
-        }
-        catch (IOException e)
+        } catch (IOException e)
         {
-            Log.e("OC Music Events", "Error loading image: " + imageFileName, e);
+            Log.e("OC Music Events", "Error loading image: " + imageName, e);
         }
         eventTitleTextView.setText(title);
-        eventDetailsTextView.setText(details);
-
+        eventDateDayTextView.setText(date + " - " + day);
+        eventTimeTextView.setText(time);
+        eventLocationTextView.setText(location);
+        eventAddress1TextView.setText(address1);
+        eventAddress2TextView.setText(address2);
 
     }
 
+    /**
+     * Terminates this activity and returns to <code>EventsListActivity</code>.
+     *
+     * @param v The <code>View</code> that called this method.
+     */
     protected void goBackToList(View v)
     {
         finish();
