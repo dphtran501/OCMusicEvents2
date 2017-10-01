c6df23fb984452e9e8c2e76b171de6de69389c2f
Derek <dphtran501@yahoo.com>
2017-09-30 22:39:31 -0700

    added javadocs to MusicEvent.java

--- app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/MusicEvent.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/MusicEvent.java
@@ -8,7 +8,8 @@ package edu.orangecoastcollege.cs273.ocmusicevents2;
  * @author Michael Paulding
  * @version 1.0
  */
-public class MusicEvent {
+public class MusicEvent
+{
 
     private String mTitle;
     private String mDate;
@@ -19,67 +20,163 @@ public class MusicEvent {
     private String mAddress2;
     private String mImageName;
 
-    public String getTitle() {
+    /**
+     * Gets the title of the music event.
+     *
+     * @return Title of the music event.
+     */
+    public String getTitle()
+    {
         return mTitle;
     }
 
-    public void setTitle(String title) {
+    /**
+     * Sets the title of the music event.
+     *
+     * @param title Title of the music event.
+     */
+    public void setTitle(String title)
+    {
         mTitle = title;
     }
 
-    public String getDate() {
+    /**
+     * Gets the date of the music event.
+     *
+     * @return The date of the music event.
+     */
+    public String getDate()
+    {
         return mDate;
     }
 
-    public void setDate(String date) {
+    /**
+     * Sets the date of the music event.
+     *
+     * @param date The date of the music event.
+     */
+    public void setDate(String date)
+    {
         mDate = date;
     }
 
-    public String getDay() {
+    /**
+     * Gets the day of the music event.
+     *
+     * @return The day of the music event.
+     */
+    public String getDay()
+    {
         return mDay;
     }
 
-    public void setDay(String day) {
+    /**
+     * Sets the day of the music event.
+     *
+     * @param day The day of the music event.
+     */
+    public void setDay(String day)
+    {
         mDay = day;
     }
 
-    public String getTime() {
+    /**
+     * Gets the time of the music event.
+     *
+     * @return The time of the music event.
+     */
+    public String getTime()
+    {
         return mTime;
     }
 
-    public void setTime(String time) {
+    /**
+     * GSets the time of the music event.
+     *
+     * @param time The time of the music event.
+     */
+    public void setTime(String time)
+    {
         mTime = time;
     }
 
-    public String getLocation() {
+    /**
+     * Gets the location of the music event.
+     *
+     * @return The location of the music event.
+     */
+    public String getLocation()
+    {
         return mLocation;
     }
 
-    public void setLocation(String location) {
+    /**
+     * Sets the location of the music event.
+     *
+     * @param location The location of the music event.
+     */
+    public void setLocation(String location)
+    {
         mLocation = location;
     }
 
-    public String getAddress1() {
+    /**
+     * Gets the street address of the music event.
+     *
+     * @return The street address of the music event.
+     */
+    public String getAddress1()
+    {
         return mAddress1;
     }
 
-    public void setAddress1(String address1) {
+    /**
+     * Sets the street address of the music event.
+     *
+     * @param address1 The street address of the music event.
+     */
+    public void setAddress1(String address1)
+    {
         mAddress1 = address1;
     }
 
-    public String getAddress2() {
+    /**
+     * Gets the city address of the music event.
+     *
+     * @return The city address of the music event.
+     */
+    public String getAddress2()
+    {
         return mAddress2;
     }
 
-    public void setAddress2(String address2) {
+    /**
+     * Sets the city address of the music event.
+     *
+     * @param address2 The city address of the music event.
+     */
+    public void setAddress2(String address2)
+    {
         mAddress2 = address2;
     }
 
-    public String getImageName() {
+    /**
+     * Gets the image name of the music event image.
+     *
+     * @return The image name of the music event image.
+     */
+    public String getImageName()
+    {
         return mImageName;
     }
 
-    public void setImageName(String imageName) {
+    /**
+     * Sets the image name of the music event image.
+     *
+     * @param imageName The image name of the music event image.
+     */
+    public void setImageName(String imageName)
+    {
         mImageName = imageName;
     }
 }
