5a25e209f9bb522de84141fdc902914e86329700
Derek <dphtran501@yahoo.com>
2017-09-30 23:22:44 -0700

    added javadocs to EventListAdapter.java

--- app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventListAdapter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventListAdapter.java
@@ -19,7 +19,11 @@ import java.io.InputStream;
 import java.util.List;
 
 /**
- * Created by Jeannie on 9/26/2017.
+ * An Adapter object that provides views for each MusicEvent object in an MusicEvent data set.
+ *
+ * @author Derek Tran
+ * @version 1.0
+ * @since September 26, 2017
  */
 
 public class EventListAdapter extends ArrayAdapter<MusicEvent>
@@ -31,6 +35,13 @@ public class EventListAdapter extends ArrayAdapter<MusicEvent>
     // context = Activity that uses the adapter (EventsListActivity)
     // resource = layout file to inflate (R.layout.music_event_list_item)
     // objects = List<MusicEvent>
+
+    /**
+     * Creates a new EventListAdapter object.
+     * @param context The activity that uses this adapter.
+     * @param resource The layout file to inflate.
+     * @param allMusicEvents A list of MusicEvent objects.
+     */
     public EventListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<MusicEvent> allMusicEvents)
     {
         super(context, resource, allMusicEvents);
@@ -41,6 +52,23 @@ public class EventListAdapter extends ArrayAdapter<MusicEvent>
 
     // Override method called getView
 
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * inflate(int, android.view.ViewGroup, boolean) to specify a root view and to prevent
+     * attachment to the root.
+     * @param position The position of the item within the adapter's data set of the item whose view
+     *                 we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible
+     *                    to convert this view to display the correct data, this method can create a
+     *                    new view. Heterogeneous lists can specify their number of view types, so
+     *                    that this View is always of the right type (see getViewTypeCount() and
+     *                    getItemViewType(int)).
+     * @param parent The parent that this view will eventually be attached to.
+     * @return A View corresponding to the data at the specified position.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
